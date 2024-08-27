e6e65a6f57d27d549c3c4d005abf3a2673e449d7
pawanomprakash <141708273+pawanomprakash@users.noreply.github.com>
2024-08-28 02:23:01 +0530

    added basic functionalities for player and questions and recommendations file

--- Player.h
+++ Player.h
@@ -1,18 +1,10 @@
-#include <iostream>
-#include <cmath>
-#include <algorithm>
-#include <iomanip>
-#include <string>
-
-using namespace std;
-
 class Player {
 public:
     string name;
     int score;
 
-    Player(string name) {
-        this->name = name;
+    Player(string n) {
+        this->name = n;
         this->score = 0;
     }
 
@@ -23,5 +15,14 @@ public:
     int getScore() {
         return score;
     }
-};
 
+    void giveFeedback() {
+        if (score >= 30) {
+            cout << "Excellent job, " << name << "! You're a movie buff!" << endl;
+        } else if (score >= 10) {
+            cout << "Good effort, " << name << "! Keep watching more movies!" << endl;
+        } else {
+            cout << "It looks like you need to brush up on your movie knowledge, " << name << "!" << endl;
+        }
+    }
+};
